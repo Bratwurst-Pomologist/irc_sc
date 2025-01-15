f57d3fd4035ab7a0ac7d85efc3170891516c9c8a
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-01-15 20:30:14 +0100

    update code.lua

--- code.lua
+++ code.lua
@@ -8,3 +8,26 @@ minetest.register_chatcommand("irc_sc", {
   
   
   
+-- irc_sc/init.lua
+
+minetest.register_chatcommand("irc_sc", {
+    params = "<action> <playername>",
+    description = "IRC Server Control",
+    func = function(name, param)
+        local args = param:split(" ")
+        if #args < 2 then
+            return false, "Usage: /irc_sc <action> <playername>"
+        end
+        local action = args[1]
+        local playername = args[2]
+
+        if action == "kick" then
+            local irc_command = "irc_quote kick #Catlandia " .. playername
+            minetest.chat_send_all("Executing IRC command: " .. irc_command)
+        else
+            return false, "Unknown action: " .. action
+        end
+
+        return true, "Command executed."
+    end,
+})
