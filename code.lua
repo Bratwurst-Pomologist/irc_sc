2b1366a7e64e2e84c454263235d5d6a16f751dcf
2BW <71567744+Bratwurst-Pomologist@users.noreply.github.com>
2025-01-15 20:13:36 +0100

    update code.lua

---
+++ code.lua
@@ -0,0 +1,10 @@
+minetest.register_chatcommand("irc_sc", {
+  description = "kick or ban people from IRC"
+  privs = {server = true},
+  params = "[task] [playername]",
+  func = funktion(name)
+  end,
+  })
+  
+  
+  
